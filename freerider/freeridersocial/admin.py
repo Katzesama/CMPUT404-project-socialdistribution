666156e289d1b32671104b4592423535b7a21969
Katzesama <31713290+Katzesama@users.noreply.github.com>
2019-03-08 14:25:01 -0700

    register user

--- freerider/freeridersocial/admin.py
+++ freerider/freeridersocial/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.models import User
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Post)
 admin.site.register(Images)
 admin.site.register(Comment)
 admin.site.register(Friend)
+admin.site.regitster(User)
