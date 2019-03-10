741594e0c1127b725d46295e4b399496cb60395a
Katzesama <31713290+Katzesama@users.noreply.github.com>
2019-03-09 18:57:11 -0700

    Update settings.py

--- freerider/freerider/settings.py
+++ freerider/freerider/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'freerider.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../freeridersocial/Templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'freeridersocial/Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
