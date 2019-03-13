7eecc61ca5a18d27b4b8061965eb46dde686aae2
xinyizhuang <xzhuang1@ualberta.ca>
2019-03-13 15:48:07 -0600

    login/logout test

--- freerider/freeridersocial/tests.py
+++ freerider/freeridersocial/tests.py
@@ -2,28 +2,31 @@ from django.test import TestCase
 from django.test import SimpleTestCase
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
+from rest_framework.test import APIClient
+
 # Create your tests here.
 
 class loginTest(TestCase):
 
-    def setUp(self):
+    def setUp(self):       
         password = make_password("12202stst")
-        user = User.objects.create(username="TestUser", password=password)
+        User.objects.create(username="TestUser", password=password)
 
     def test_login_success(self):
-
-        response = self.client.login(username='TestUser', password='12202stst')
+        client = APIClient()
+        response = client.login(username='TestUser', password='12202stst')
         self.assertTrue(response)
 
     def test_login_failed(self):
-
-        response = self.client.login(username='TestUser', password='fake12202stst')
+        client = APIClient()
+        response = client.login(username='TestUser', password='fake12202stst')
         self.assertFalse(response)
 
     def test_logout(self):
-        self.client.login(username='TestUser', password='12202stst')
-        response = self.client.logout()
-
+        client = APIClient()
+        client.login(username='TestUser', password='12202stst')
+        # client.logout()
+        response = client.get("/profile/")
         self.assertNotEqual(response.status_code, 200)
 
 
