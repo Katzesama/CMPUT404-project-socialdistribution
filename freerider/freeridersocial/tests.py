8991f2c1740c0d24a95215d90267418b83cad712
xinyizhuang <xzhuang1@ualberta.ca>
2019-03-13 14:02:51 -0600

    .

--- freerider/freeridersocial/tests.py
+++ freerider/freeridersocial/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+# from django.test import TestCase
+# from django.test import Client
+# from django.contrib.auth.models import User
+# from rest_framework.test import APIClient
 
+from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 # Create your tests here.
+
+class loginTest(TestCase):
+
+    def setup(self):
+        self.client = Client()
+
+    def test_logint(self):
+        response = self.client.get("{% url 'login' %})
+        self.assertEquals(response.status_code, 200)
+
