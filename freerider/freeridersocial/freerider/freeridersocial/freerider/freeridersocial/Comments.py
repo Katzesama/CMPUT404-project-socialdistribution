d594d0d6d67646814a28aa4f863c69e396015937
Wenhan Yang <31970257+wyang2@users.noreply.github.com>
2019-03-26 18:57:49 -0600

    profile api

---
+++ freerider/freeridersocial/freerider/freeridersocial/freerider/freeridersocial/Comments.py
@@ -0,0 +1,60 @@
+from .models import *
+from .serializer import *
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404, redirect
+from .serializer import AuthorSerializer, FriendSerializer
+from django.http import HttpResponse, JsonResponse
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.renderers import JSONRenderer
+import uuid
+
+
+# http://service/posts/{post_id}/comments access to the comments in a post
+# "query": "addComment"
+
+class addComment(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'addcomment.html'
+    def get(self, request, post_id, **kwargs):
+        try:
+            current_user_profile = request.user.author
+            post = get_object_or_404(Post, pk = post_id)
+        except:
+            return HttpResponse(status=404)
+        new_comment = Comment.objects.create(post_id=post, author=current_user_profile)
+        request.session["Comment_id"] = str(new_comment.id)
+        serializer = CommentSerializer(new_comment)
+        return Response({"serializer": serializer})
+
+    def post(self, request, post_id, **kwargs):
+
+        post = get_object_or_404(Post, pk = post_id)
+        try:
+            id = uuid.UUID(request.session['Comment_id']).hex
+            new_comment = Comment.objects.get(id=id)
+        except:
+            new_comment = Comment.objects.create(post_id = post, author=request.user.author)
+        serializer = CommentSerializer(new_comment, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return redirect("get_one_post", post.id)
+
+        print("awsl")
+        print(serializer.errors)
+        return Response({'serializer': serializer})
+
+# POST to http://service/posts/{POST_ID}/comments
+class get_comments(APIView):
+    def get(self, request, post_id, **kwargs):
+        try:
+            post = get_object_or_404(Post, pk = post_id)
+        except:
+            return HttpResponse(status=404)
+        comment_list = Comment.objects.filter(post_id=post)
+        pg_obj = PaginationModel()
+        pg_res = pg_obj.paginate_queryset(queryset=comment_list, request=request)
+        res = CommentSerializer(pg_res, many=True)
+        print(res.data)
+        return pg_obj.get_paginated_response(res.data)
---
+++ freerider/freeridersocial/freerider/freeridersocial/freerider/freeridersocial/models.py
@@ -0,0 +1,90 @@
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+import uuid
+import json
+
+# Profile With User
+class Author(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, editable=False)
+    host = models.URLField()
+    displayName = models.CharField(max_length=200,blank=False,null=False)
+    url = models.URLField(editable=False)
+    github = models.URLField(null=True, blank=False)
+    firstName = models.CharField(max_length=200,blank=True,default='')
+    lastName = models.CharField(max_length=200,blank=True,default='')
+    email = models.CharField(max_length=400,blank=True,default='')
+    bio = models.CharField(max_length=2000,blank=True,default='')
+    image = models.ImageField(default='default.img', upload_to="profile_pics")
+
+    def __str__(self):  # __unicode__ for Python 2
+        return f'{self.user.username} Profile'
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length = 100, default='No Title')
+    source = models.CharField(max_length = 2000)
+    origin = models.CharField(max_length = 2000, editable=False)
+    description = models.CharField(max_length =100)
+    contentType_choice = (
+        ('text/markdown', 'text/markdown'),
+        ('text/plain', 'text/plain'),
+        ('application/base64', 'application/base65'),
+        ('image/png;base64', 'image/png;base64'),
+        ('image/jpeg;base64', 'image/jpeg;base64'),
+    )
+    image = models.ImageField(null=True, blank=True)
+    contentType = models.CharField(max_length=2000, choices=contentType_choice, default='text/markdown')
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    published = models.DateTimeField(default=datetime.now)
+    categories = models.TextField(null=True, blank=True)
+
+    visibility_choice = (
+        ('PUBLIC', 'PUBLIC'),
+        ('FOAF', 'FOAF'),
+        ('FRIENDS', 'FRIENDS'),
+        ('PRIVATE', 'PRIVATE'),
+        ('SERVERONLY', 'SERVERONLY'),
+    )
+    visibility = models.CharField(default ="PUBLIC", max_length=20, choices=visibility_choice)
+    visibleTo = models.TextField(null=True, blank=True)
+    unlisted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Images(models.Model):
+	associated_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+	img = models.ImageField(null=True, blank=True)
+
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    post_id= models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    comment =  models.TextField(max_length =2000)
+    contentType_choice = (
+        ('text/markdown', 'text/markdown'),
+        ('text/plain', 'text/plain'),
+        ('application/base64', 'application/base65'),
+        ('image/png;base64', 'image/png;base64'),
+        ('image/jpeg;base64', 'image/jpeg;base64'),
+    )
+    contentType = models.CharField(max_length=2000, default='text/plain', choices=contentType_choice)
+    published = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.author.displayName + ": " + self.comment
+
+class FriendRequest(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    displayName = models.CharField(max_length=200,blank=True)
+    host = models.URLField()
+    url = models.URLField()
+    friend_with = models.ForeignKey(Author, related_name="request_sender", on_delete=models.CASCADE)
+    friend_status = (('friend','friend'),('proceeding','proceeding'),('rejected','rejected'))
+    def __str__(self):
+        return self.displayName
