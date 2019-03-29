846168dcf9d7baa1f5452b862755dd948f572026
Katzesama <31713290+Katzesama@users.noreply.github.com>
2019-03-29 14:34:18 -0600

    Update models.py

--- freerider/freeridersocial/models.py
+++ freerider/freeridersocial/models.py
@@ -17,7 +17,7 @@ class Author(models.Model):
     lastName = models.CharField(max_length=200,blank=True,default='')
     email = models.CharField(max_length=400,blank=True,default='')
     bio = models.CharField(max_length=2000,blank=True,default='')
-    image = models.ImageField(default=null, upload_to="profile_pics")
+    image = models.ImageField(null=True, blank=True, upload_to="profile_pics")
 
     def __str__(self):  # __unicode__ for Python 2
         return f'{self.user.username} Profile'
